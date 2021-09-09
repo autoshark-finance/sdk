8667537e77d579f9cade41c751b7d81c9b944081
Anon <>
2021-09-09 18:23:08 +0800

    feat: change address

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,8 +55,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'Cake-LP',
-      'Pancake LPs'
+      'FINS-LP',
+      'Fins LPs'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
